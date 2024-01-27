8f3df5e0aaeeeed79d3383785846de55ca1bfaea
ldiaz89 <lgdt66@gmail.com>
2024-01-27 12:46:11 -0300

    delete un used imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-import { HolaComponent } from "./component/HolaComponent";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
 import { Header } from "./component/Header";
 import "./styles.css";
 import { CustomTable } from "./component/CustomTable";
-import FilterDropdown from "./component/FilterDropdown";
 function App() {
   return (
     <div className="App">
